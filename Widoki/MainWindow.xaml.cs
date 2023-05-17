a084d343c683e81b81067d9402aeddeafc862293
Dawid Dłubacz <dawiddlubacz@int.pl>
2023-05-18 00:21:05 +0200

    dodawanie zadan i kategorii

--- Widoki/MainWindow.xaml.cs
+++ Widoki/MainWindow.xaml.cs
@@ -32,13 +32,68 @@ namespace ListaZadan
             this.Context = Context;
             InitializeComponent();
             WyswietlZadania();
-            //ObservableCollection<Zadanie> zadania = new ObservableCollection<Zadanie>(lista);
+            WyswietlKategorie();
         }
 
         private void WyswietlZadania()
         {
             var listaZadan = Context.Zadania.ToList();
             ListaZadan.ItemsSource = listaZadan;
+        }
+
+        private void WyswietlKategorie()
+        {
+            var listaKategorii = Context.Kategorie.ToList();
+            ListaKategorii.ItemsSource = listaKategorii;
+            WybierzKategorie.ItemsSource = listaKategorii;
+            ListaKategorii.DisplayMemberPath = "Nazwa";
+            WybierzKategorie.DisplayMemberPath = "Nazwa";
+        }
+
+        private void DodajZadaniePole_KeyDown(object sender, KeyEventArgs e)
+        {
+            var kategoria = WybierzKategorie.SelectedItem as Kategoria;
+            var kategoriaId = kategoria.KategoriaId;
+
+            if (e.Key == Key.Enter)
+            {
+                var zadanie = new Zadanie
+                {
+                    Nazwa = DodajZadaniePole.Text,
+                    KategoriaId = kategoriaId
+                };
+
+                Context.Zadania.Add(zadanie);
+                Context.SaveChanges();
+                WyswietlZadania();
+                DodajZadaniePole.Text = "";
+            }
+        }
+
+        private void DodajZadaniePole_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DodajZadaniePole.Text = "";
+        }
+
+        private void DodajKategoriePole_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DodajKategoriePole.Text = "";
+        }
+
+        private void DodajKategoriePole_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var kategoria = new Kategoria
+                {
+                    Nazwa = DodajKategoriePole.Text
+                };
+
+                Context.Kategorie.Add(kategoria);
+                Context.SaveChanges();
+                WyswietlKategorie();
+                DodajKategoriePole.Text = "";
+            }
         }
     }
 }
